e514537672018b43ebf5089e46d8aa01409260ec
jason <13521008@std.stei.itb.ac.id>
2023-12-01 14:52:29 +0700

    feat: add aboutus page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Dashboard } from './component/Dashboard'
 import { Generate } from './component/Generate'
+import { AboutUs } from './component/AboutUs'
 
 export const App = () => (
   <Router>
     <Routes>
       <Route path="/" element={<Dashboard />} />
       <Route path="/generate" element={<Generate />} />
+      <Route path="/aboutus" element={<AboutUs />} />
     </Routes>
   </Router>
 )
---
+++ src/component/AboutUs.tsx
@@ -0,0 +1,134 @@
+import { Heading, Text, Flex, Box, Avatar } from '@chakra-ui/react'
+import Navbar from '../component/Navbar/Navbar'
+
+export const AboutUs = () => {
+  return (
+    <>
+      <Navbar />
+      <Flex
+        direction="column"
+        justifyContent="center"
+        h="91.2vh"
+        px={52}
+        bgGradient="linear(to-r, red.200, yellow.500)"
+      >
+        <Flex direction="row">
+          <Avatar
+            width={220}
+            height={220}
+            src="https://media.licdn.com/dms/image/D4E03AQFk2CNS0I1IWg/profile-displayphoto-shrink_400_400/0/1691679286411?e=1706745600&v=beta&t=R0fAw0fgyMQpY9LSe2yySvkyZM4n9UwvBkxns9Uk7Kg"
+            position="relative"
+            zIndex={2}
+            border="20px solid #BBDEFB"
+            mb={-20}
+            mr={20}
+          />
+          <Avatar
+            width={220}
+            height={220}
+            src="https://media.licdn.com/dms/image/D5603AQEE9HQ1cd6rYQ/profile-displayphoto-shrink_400_400/0/1686828145381?e=1706745600&v=beta&t=XyZUmzstNHlB1NzE2Rv-9iEeZf4QWigjQP9oTfTx8ZE"
+            position="relative"
+            zIndex={2}
+            border="20px solid #BBDEFB"
+            mb={-20}
+            mr={20}
+          />
+          <Avatar
+            width={220}
+            height={220}
+            src="https://avatars.githubusercontent.com/u/88835532?v=4"
+            position="relative"
+            zIndex={2}
+            border="20px solid #BBDEFB"
+            mb={-20}
+            mr={20}
+          />
+          <Avatar
+            width={220}
+            height={220}
+            position="relative"
+            zIndex={2}
+            border="20px solid #BBDEFB"
+            mb={-20}
+          />
+        </Flex>
+
+        <Box
+          bg="#BBDEFB"
+          borderRadius="lg"
+          p={8}
+          boxShadow="lg"
+          mt={4}
+          width="100%"
+          height={180}
+        >
+          <Flex direction="row" mt={12}>
+            <Flex direction="column" align="center" ml={4} mr={32}>
+              <Heading
+                as="h2"
+                size="md"
+                fontWeight="bold"
+                color="black"
+                textAlign={'center'}
+                mb={2}
+              >
+                Jason Rivalino
+              </Heading>
+              <Text fontSize="sm" fontWeight="bold" textAlign={'center'}>
+                13521008
+              </Text>
+            </Flex>
+
+            <Flex direction="column" align="center" ml={2} mr={36}>
+              <Heading
+                as="h2"
+                size="md"
+                fontWeight="bold"
+                color="black"
+                textAlign={'center'}
+                mb={2}
+              >
+                Muhammad Salman<br></br>Hakim Alfarisi
+              </Heading>
+              <Text fontSize="sm" fontWeight="bold" textAlign={'center'}>
+                13521010
+              </Text>
+            </Flex>
+
+            <Flex direction="column" align="center" mr={44}>
+              <Heading
+                as="h2"
+                size="md"
+                fontWeight="bold"
+                color="black"
+                textAlign={'center'}
+                mb={2}
+              >
+                Ahmad Nadil
+              </Heading>
+              <Text fontSize="sm" fontWeight="bold" textAlign={'center'}>
+                13521024
+              </Text>
+            </Flex>
+
+            <Flex direction="column" align="center">
+              <Heading
+                as="h2"
+                size="md"
+                fontWeight="bold"
+                color="black"
+                textAlign={'center'}
+                mb={2}
+              >
+                Kartini Copa
+              </Heading>
+              <Text fontSize="sm" fontWeight="bold" textAlign={'center'}>
+                13521026
+              </Text>
+            </Flex>
+          </Flex>
+        </Box>
+      </Flex>
+    </>
+  )
+}
