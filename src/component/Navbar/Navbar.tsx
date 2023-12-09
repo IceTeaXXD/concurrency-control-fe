b806263d7c63fe58a90a23f86c37ae58eb924a88
Muhamad Salman Hakim Alfarisi <13521010@std.stei.itb.ac.id>
2023-12-09 14:52:23 +0700

    feat(Navbar): hamburger details

--- src/component/Navbar/Navbar.tsx
+++ src/component/Navbar/Navbar.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import {
   Box,
   Flex,
@@ -8,32 +9,61 @@ import {
   MenuButton,
   MenuList,
   MenuItem,
-} from '@chakra-ui/react'
-import { Link } from 'react-router-dom'
+  IconButton,
+  Drawer,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  DrawerHeader,
+  DrawerBody,
+} from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+import { HamburgerIcon } from '@chakra-ui/icons';
 
 const Navbar = () => {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  const handleDrawerOpen = () => {
+    setIsDrawerOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setIsDrawerOpen(false);
+  };
+
   return (
     <ChakraProvider>
-      <Flex p="5" bg="purple.500" color="white">
+      <Flex p="5" bg="purple.500" color="white" justifyContent="flex-end">
         <Spacer />
-        <Box>
+
+        {/* Hamburger Icon for Mobile */}
+        <Box display={{ base: 'block', md: 'none' }}>
+          <IconButton
+            icon={<HamburgerIcon />}
+            onClick={handleDrawerOpen}
+            aria-label="Open navigation drawer"
+          />
+        </Box>
+
+        {/* Regular navigation links for larger screens */}
+        <Box display={{ base: 'none', md: 'block' }} mr={10}>
           <Link to="/">
-            <Text fontSize="md" fontWeight="bold" mr={10}>
+            <Text fontSize="md" fontWeight="bold">
               Home
             </Text>
           </Link>
         </Box>
-        <Box>
+        <Box display={{ base: 'none', md: 'block' }} mr={10}>
           <Link to="/generate">
-            <Text fontSize="md" fontWeight="bold" mr={10}>
+            <Text fontSize="md" fontWeight="bold">
               Generate
             </Text>
           </Link>
         </Box>
-        <Box>
+        <Box display={{ base: 'none', md: 'block' }} mr={10}>
           <Menu>
             <MenuButton as={Box} cursor="pointer">
-              <Text fontSize="md" fontWeight="bold" mr={10}>
+              <Text fontSize="md" fontWeight="bold">
                 Reference
               </Text>
             </MenuButton>
@@ -47,16 +77,65 @@ const Navbar = () => {
             </MenuList>
           </Menu>
         </Box>
-        <Box>
+        <Box display={{ base: 'none', md: 'block' }} mr={2}>
           <Link to="/aboutus">
-            <Text fontSize="md" fontWeight="bold" mr={2}>
+            <Text fontSize="md" fontWeight="bold">
               About Us
             </Text>
           </Link>
         </Box>
+
+        {/* Drawer for Mobile */}
+        <Drawer isOpen={isDrawerOpen} onClose={handleDrawerClose} placement="right">
+          <DrawerOverlay />
+          <DrawerContent>
+            <DrawerCloseButton />
+            <DrawerHeader color="purple.500"
+            >Menu</DrawerHeader>
+            <DrawerBody>
+              {/* Navigation links in the drawer */}
+              <Box>
+                <Link to="/">
+                  <Text fontSize="md" fontWeight="bold" mb={4} display="block" color={'purple.500'}>
+                    Home
+                  </Text>
+                </Link>
+              </Box>
+              <Box>
+                <Link to="/generate">
+                  <Text fontSize="md" fontWeight="bold" mb={4} display="block" color={'purple.500'}>
+                    Generate
+                  </Text>
+                </Link>
+              </Box>
+              <Menu>
+                <MenuButton as={Box} cursor="pointer">
+                  <Text fontSize="md" fontWeight="bold" mb={4} display="block" color={'purple.500'}>
+                    Reference
+                  </Text>
+                </MenuButton>
+                <MenuList color="purple.500">
+                  <MenuItem _hover={{ bg: 'purple.400', color: 'white' }}>
+                    <a href="#">Source Code</a>
+                  </MenuItem>
+                  <MenuItem _hover={{ bg: 'purple.400', color: 'white' }}>
+                    <a href="#">Document</a>
+                  </MenuItem>
+                </MenuList>
+              </Menu>
+              <Box>
+                <Link to="/aboutus">
+                  <Text fontSize="md" fontWeight="bold" mb={4} display="block" color={'purple.500'}>
+                    About Us
+                  </Text>
+                </Link>
+              </Box>
+            </DrawerBody>
+          </DrawerContent>
+        </Drawer>
       </Flex>
     </ChakraProvider>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
